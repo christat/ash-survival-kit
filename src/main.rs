9b00e17724f6ba350e7dd80854a920cde33441b3
Krzysztof Statkiewicz <christopherstat@protonmail.com>
2019-08-06 23:28:48 +0300

    add validation layers (pray there's no lurking bugs)

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,8 @@
 use std::{
     default::Default,
     error::Error,
-    ffi::CString,
-    fmt,
+    ffi::{CStr, CString},
+    os::raw::{c_char, c_void},
     ptr
 };
 
@@ -10,7 +10,12 @@ extern crate ash;
 use ash::{
     vk,
     Entry,
+    extensions::{
+        ext::DebugUtils,
+        khr::{Surface, Win32Surface}
+    },
     Instance,
+    prelude::VkResult,
     version::{EntryV1_0, InstanceV1_0},
 };
 
@@ -23,26 +28,42 @@ use winit::{
     window::{Window, WindowBuilder},
 };
 
+mod utils;
+use utils::debugging;
+
+#[cfg(debug_assertions)]
+const ENABLE_VALIDATION_LAYERS: bool = true;
+#[cfg(not(debug_assertions))]
+const ENABLE_VALIDATION_LAYERS: bool = false;
+
 const WINDOW_WIDTH: usize = 800;
 const WINDOW_HEIGHT: usize = 600;
 
-struct HelloTriangleApplication {}
+struct HelloTriangleApplication {
+    entry: Entry,
+    instance: Instance,
+    debug_utils: DebugUtils,
+    debug_utils_messenger_ext: vk::DebugUtilsMessengerEXT
+}
 
 impl HelloTriangleApplication {
     pub fn new() -> Self {
-        Self {}
+        let (entry, instance)= Self::init_vulkan();
+        let (debug_utils, debug_utils_messenger_ext) = Self::setup_debug_messenger(&entry, &instance);
+        Self {
+            entry,
+            instance,
+            debug_utils,
+            debug_utils_messenger_ext
+        }
     }
 
     pub fn run (&mut self) -> Result<(), Box<dyn Error>> {
-        let (event_loop, window) = self.init_window();
-        let instance= self.init_vulkan();
-
-        self.main_loop(event_loop, window);
-        self.cleanup(instance);
+        self.main_loop();
         Ok(())
     }
 
-    fn init_window(&mut self) -> (EventLoop<()>, Window) {
+    fn init_window() -> (EventLoop<()>, Window) {
         let event_loop = EventLoop::new();
         let window = WindowBuilder::new()
             .with_inner_size(LogicalSize::new(WINDOW_WIDTH as f64, WINDOW_HEIGHT as f64))
@@ -55,15 +76,20 @@ impl HelloTriangleApplication {
         }
     }
 
-    fn init_vulkan(&self) -> Instance {
-        let instance_result = self.create_vk_instance();
+    fn init_vulkan() -> (Entry, Instance) {
+        let instance_result = Self::create_vk_instance();
         match instance_result {
-            Ok((_entry, instance)) => instance,
+            Ok((entry, instance)) => (entry, instance),
             Err(e) => panic!("Failed to create Vulkan instance: {}", e)
         }
     }
 
-    fn create_vk_instance(&self) -> Result<(Entry, Instance), Box<dyn Error>> {
+    fn create_vk_instance() -> Result<(Entry, Instance), Box<dyn Error>> {
+        let entry= Entry::new()?;
+        if ENABLE_VALIDATION_LAYERS && !debugging::check_validation_layer_support(&entry) {
+            panic!("Validation layers requested but not available!")
+        }
+
         let application_name = CString::new("Hello triangle").unwrap();
         let engine_name = CString::new("No engine").unwrap();
         let version = ash::vk_make_version!(1, 0, 0);
@@ -76,16 +102,79 @@ impl HelloTriangleApplication {
             .api_version(version)
             .build();
 
-        let instance_create_info = vk::InstanceCreateInfo::builder()
-            .application_info(&application_info)
-            .build();
+        // variables below in main function body to prevent getting destroyed before entry.create_instance()
+        let enabled_layer_names = debugging::get_enabled_layer_names();
+        let enabled_layer_names: Vec<*const c_char> = enabled_layer_names
+            .iter()
+            .map(|layer_name| layer_name.as_ptr())
+            .collect();
+        let enabled_extension_names = Self::get_required_extensions();
+        let create_info = Self::populate_debug_messenger_create_info();
 
-        let entry= Entry::new()?;
+        let mut instance_create_info_builder = vk::InstanceCreateInfo::builder().application_info(&application_info);
+        if ENABLE_VALIDATION_LAYERS {
+            instance_create_info_builder = instance_create_info_builder
+                .enabled_layer_names(&enabled_layer_names)
+                .enabled_extension_names(&enabled_extension_names);
+        }
+        let mut instance_create_info = instance_create_info_builder.build();
+        if ENABLE_VALIDATION_LAYERS {
+            instance_create_info.p_next = &create_info as *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void;
+        }
+
         let instance = unsafe { entry.create_instance(&instance_create_info, None)? };
         Ok((entry, instance))
     }
 
-    fn main_loop(&mut self, event_loop: EventLoop<()>, window: Window) {
+    fn get_required_extensions() -> Vec<*const c_char> {
+        vec![
+            Surface::name().as_ptr(),
+            Win32Surface::name().as_ptr(),
+            DebugUtils::name().as_ptr()
+        ]
+    }
+
+    fn setup_debug_messenger(entry: &Entry, instance: &Instance) -> (DebugUtils, vk::DebugUtilsMessengerEXT){
+        let debug_utils = DebugUtils::new(entry, instance);
+
+        if !ENABLE_VALIDATION_LAYERS {
+            return (debug_utils, vk::DebugUtilsMessengerEXT::null())
+        }
+
+        let create_info = Self::populate_debug_messenger_create_info();
+
+        unsafe {
+            let debug_utils_messenger_ext = DebugUtils::create_debug_utils_messenger(
+                &debug_utils,
+                &create_info,
+                None
+            ).expect("Failed to create DebugUtilsMessengerEXT");
+            (debug_utils, debug_utils_messenger_ext)
+        }
+    }
+
+    fn populate_debug_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
+        vk::DebugUtilsMessengerCreateInfoEXT {
+            s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
+            message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            pfn_user_callback: Some(Self::debug_callback),
+            ..Default::default()
+        }
+    }
+
+    unsafe extern "system" fn debug_callback(
+        message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
+        message_types: vk::DebugUtilsMessageTypeFlagsEXT,
+        p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
+        p_user_data: *mut c_void
+    ) -> vk::Bool32 {
+        unsafe { eprintln!("validation layer: {}", CStr::from_ptr((*p_callback_data).p_message).to_str().unwrap()); }
+        vk::FALSE
+    }
+
+    fn main_loop(&mut self) {
+        let (event_loop, window) = Self::init_window();
         event_loop.run(move |event, _, control_flow| {
             match event {
                 Event::WindowEvent {
@@ -96,9 +185,15 @@ impl HelloTriangleApplication {
             }
         });
     }
+}
 
-    fn cleanup(&self, instance: Instance) {
-        unsafe { instance.destroy_instance(None) };
+impl Drop for HelloTriangleApplication {
+    fn drop(&mut self) {
+        if ENABLE_VALIDATION_LAYERS {
+            unsafe { self.debug_utils.destroy_debug_utils_messenger(self.debug_utils_messenger_ext, None) };
+        }
+
+        unsafe { self.instance.destroy_instance(None) };
     }
 }
 
---
+++ src/utils/debugging.rs
@@ -0,0 +1,29 @@
+use std::ffi::{CStr, CString};
+
+use ash::{
+    Entry,
+    version::EntryV1_0
+};
+
+const VALIDATION_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
+
+pub fn check_validation_layer_support(entry: &Entry) -> bool {
+    let layer_properties = match entry.enumerate_instance_layer_properties() {
+        Ok(layer_properties) => layer_properties,
+        Err(e) => panic!("Failed to enumerate instance layer properties: {}", e)
+    };
+    let layer_names = layer_properties.iter().map(|lp| unsafe { CStr::from_ptr(lp.layer_name.as_ptr()).to_str().unwrap() }).collect::<Vec<&str>>();
+    for validation_layer in VALIDATION_LAYERS.iter() {
+        if !layer_names.contains(validation_layer) {
+            return false;
+        }
+    }
+    true
+}
+
+pub fn get_enabled_layer_names() -> Vec<CString> {
+   VALIDATION_LAYERS
+    .iter()
+    .map(|layer_name| CString::new(*layer_name).unwrap())
+    .collect()
+}
