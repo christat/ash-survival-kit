63bd7f31f47e8777e60d5b10f35e3a07ee9d2cdb
Krzysztof Statkiewicz <christopherstat@protonmail.com>
2019-08-07 15:33:58 +0300

    select physical device

--- src/main.rs
+++ src/main.rs
@@ -3,20 +3,18 @@ use std::{
     error::Error,
     ffi::{CStr, CString},
     os::raw::{c_char, c_void},
-    ptr
+    ptr,
 };
 
 extern crate ash;
 use ash::{
-    vk,
-    Entry,
     extensions::{
         ext::DebugUtils,
-        khr::{Surface, Win32Surface}
+        khr::{Surface, Win32Surface},
     },
-    Instance,
     prelude::VkResult,
     version::{EntryV1_0, InstanceV1_0},
+    vk, Entry, Instance,
 };
 
 extern crate winit;
@@ -43,22 +41,24 @@ struct HelloTriangleApplication {
     entry: Entry,
     instance: Instance,
     debug_utils: DebugUtils,
-    debug_utils_messenger_ext: vk::DebugUtilsMessengerEXT
+    debug_utils_messenger_ext: vk::DebugUtilsMessengerEXT,
 }
 
 impl HelloTriangleApplication {
     pub fn new() -> Self {
-        let (entry, instance)= Self::init_vulkan();
-        let (debug_utils, debug_utils_messenger_ext) = Self::setup_debug_messenger(&entry, &instance);
+        let (entry, instance) = Self::init_vulkan();
+        let (debug_utils, debug_utils_messenger_ext) =
+            Self::setup_debug_messenger(&entry, &instance);
+        let physical_device = Self::select_physical_device(&entry, &instance);
         Self {
             entry,
             instance,
             debug_utils,
-            debug_utils_messenger_ext
+            debug_utils_messenger_ext,
         }
     }
 
-    pub fn run (&mut self) -> Result<(), Box<dyn Error>> {
+    pub fn run(&mut self) -> Result<(), Box<dyn Error>> {
         self.main_loop();
         Ok(())
     }
@@ -72,7 +72,7 @@ impl HelloTriangleApplication {
 
         match window {
             Ok(window) => (event_loop, window),
-            Err(e) => panic!("Failed to create window: {}", e)
+            Err(e) => panic!("Failed to create window: {}", e),
         }
     }
 
@@ -80,12 +80,12 @@ impl HelloTriangleApplication {
         let instance_result = Self::create_vk_instance();
         match instance_result {
             Ok((entry, instance)) => (entry, instance),
-            Err(e) => panic!("Failed to create Vulkan instance: {}", e)
+            Err(e) => panic!("Failed to create Vulkan instance: {}", e),
         }
     }
 
     fn create_vk_instance() -> Result<(Entry, Instance), Box<dyn Error>> {
-        let entry= Entry::new()?;
+        let entry = Entry::new()?;
         if ENABLE_VALIDATION_LAYERS && !debugging::check_validation_layer_support(&entry) {
             panic!("Validation layers requested but not available!")
         }
@@ -111,15 +111,18 @@ impl HelloTriangleApplication {
         let enabled_extension_names = Self::get_required_extensions();
         let create_info = Self::populate_debug_messenger_create_info();
 
-        let mut instance_create_info_builder = vk::InstanceCreateInfo::builder().application_info(&application_info);
+        let mut instance_create_info_builder =
+            vk::InstanceCreateInfo::builder().application_info(&application_info);
         if ENABLE_VALIDATION_LAYERS {
             instance_create_info_builder = instance_create_info_builder
                 .enabled_layer_names(&enabled_layer_names)
                 .enabled_extension_names(&enabled_extension_names);
         }
+
         let mut instance_create_info = instance_create_info_builder.build();
         if ENABLE_VALIDATION_LAYERS {
-            instance_create_info.p_next = &create_info as *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void;
+            instance_create_info.p_next =
+                &create_info as *const vk::DebugUtilsMessengerCreateInfoEXT as *const c_void;
         }
 
         let instance = unsafe { entry.create_instance(&instance_create_info, None)? };
@@ -130,25 +133,26 @@ impl HelloTriangleApplication {
         vec![
             Surface::name().as_ptr(),
             Win32Surface::name().as_ptr(),
-            DebugUtils::name().as_ptr()
+            DebugUtils::name().as_ptr(),
         ]
     }
 
-    fn setup_debug_messenger(entry: &Entry, instance: &Instance) -> (DebugUtils, vk::DebugUtilsMessengerEXT){
+    fn setup_debug_messenger(
+        entry: &Entry,
+        instance: &Instance,
+    ) -> (DebugUtils, vk::DebugUtilsMessengerEXT) {
         let debug_utils = DebugUtils::new(entry, instance);
 
         if !ENABLE_VALIDATION_LAYERS {
-            return (debug_utils, vk::DebugUtilsMessengerEXT::null())
+            return (debug_utils, vk::DebugUtilsMessengerEXT::null());
         }
 
         let create_info = Self::populate_debug_messenger_create_info();
 
         unsafe {
-            let debug_utils_messenger_ext = DebugUtils::create_debug_utils_messenger(
-                &debug_utils,
-                &create_info,
-                None
-            ).expect("Failed to create DebugUtilsMessengerEXT");
+            let debug_utils_messenger_ext =
+                DebugUtils::create_debug_utils_messenger(&debug_utils, &create_info, None)
+                    .expect("Failed to create DebugUtilsMessengerEXT");
             (debug_utils, debug_utils_messenger_ext)
         }
     }
@@ -156,8 +160,12 @@ impl HelloTriangleApplication {
     fn populate_debug_messenger_create_info() -> vk::DebugUtilsMessengerCreateInfoEXT {
         vk::DebugUtilsMessengerCreateInfoEXT {
             s_type: vk::StructureType::DEBUG_UTILS_MESSENGER_CREATE_INFO_EXT,
-            message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
-            message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
+            message_severity: vk::DebugUtilsMessageSeverityFlagsEXT::VERBOSE
+                | vk::DebugUtilsMessageSeverityFlagsEXT::WARNING
+                | vk::DebugUtilsMessageSeverityFlagsEXT::ERROR,
+            message_type: vk::DebugUtilsMessageTypeFlagsEXT::GENERAL
+                | vk::DebugUtilsMessageTypeFlagsEXT::VALIDATION
+                | vk::DebugUtilsMessageTypeFlagsEXT::PERFORMANCE,
             pfn_user_callback: Some(Self::debug_callback),
             ..Default::default()
         }
@@ -167,22 +175,80 @@ impl HelloTriangleApplication {
         message_severity: vk::DebugUtilsMessageSeverityFlagsEXT,
         message_types: vk::DebugUtilsMessageTypeFlagsEXT,
         p_callback_data: *const vk::DebugUtilsMessengerCallbackDataEXT,
-        p_user_data: *mut c_void
+        p_user_data: *mut c_void,
     ) -> vk::Bool32 {
-        unsafe { eprintln!("validation layer: {}", CStr::from_ptr((*p_callback_data).p_message).to_str().unwrap()); }
+        unsafe {
+            eprintln!(
+                "validation layer: {}",
+                CStr::from_ptr((*p_callback_data).p_message)
+                    .to_str()
+                    .unwrap()
+            );
+        }
         vk::FALSE
+    }
+
+    fn select_physical_device(entry: &Entry, instance: &Instance) {
+        let physical_devices = unsafe {
+            instance
+                .enumerate_physical_devices()
+                .expect("Failed to enumerate physical devices")
+        };
+
+        if physical_devices.len() == 0 {
+            panic!("No physical devices with Vulkan support!");
+        }
+
+        let (_score, selected_device) = physical_devices
+            .into_iter()
+            .filter_map(|device| {
+                let score = Self::get_physical_device_score(instance, device);
+                if score > 0 {
+                    Some((score, Some(device)))
+                } else {
+                    None
+                }
+            })
+            .fold((0, None), |(acc_score, acc_device), (score, device)| {
+                if score > acc_score {
+                    (score, device)
+                } else {
+                    (acc_score, acc_device)
+                }
+            });
+
+        if selected_device.is_none() {
+            panic!("No suitable devices found!")
+        }
+    }
+
+    fn get_physical_device_score(instance: &Instance, device: vk::PhysicalDevice) -> u32 {
+        let device_properties = unsafe { instance.get_physical_device_properties(device) };
+        let device_features = unsafe { instance.get_physical_device_features(device) };
+
+        // Application can't function without geometry shaders
+        if device_features.geometry_shader == 0 {
+            return 0;
+        }
+
+        let mut score = 0;
+        // Discrete GPUs have a significant performance advantage
+        if device_properties.device_type == vk::PhysicalDeviceType::DISCRETE_GPU {
+            score += 1000
+        };
+        // Maximum possible size of textures affects graphics quality
+        score += device_properties.limits.max_image_dimension2_d;
+        score
     }
 
     fn main_loop(&mut self) {
         let (event_loop, window) = Self::init_window();
-        event_loop.run(move |event, _, control_flow| {
-            match event {
-                Event::WindowEvent {
-                    event: WindowEvent::CloseRequested,
-                    window_id,
-                } if window_id == window.id() => *control_flow = ControlFlow::Exit,
-                _ => *control_flow = ControlFlow::Wait,
-            }
+        event_loop.run(move |event, _, control_flow| match event {
+            Event::WindowEvent {
+                event: WindowEvent::CloseRequested,
+                window_id,
+            } if window_id == window.id() => *control_flow = ControlFlow::Exit,
+            _ => *control_flow = ControlFlow::Wait,
         });
     }
 }
@@ -190,7 +256,10 @@ impl HelloTriangleApplication {
 impl Drop for HelloTriangleApplication {
     fn drop(&mut self) {
         if ENABLE_VALIDATION_LAYERS {
-            unsafe { self.debug_utils.destroy_debug_utils_messenger(self.debug_utils_messenger_ext, None) };
+            unsafe {
+                self.debug_utils
+                    .destroy_debug_utils_messenger(self.debug_utils_messenger_ext, None)
+            };
         }
 
         unsafe { self.instance.destroy_instance(None) };
@@ -202,6 +271,6 @@ fn main() {
     let status = app.run();
     match status {
         Ok(_) => (),
-        Err(e) => panic!("Application crashed! Trace: {}", e)
+        Err(e) => panic!("Application crashed! Trace: {}", e),
     }
 }
--- src/utils/debugging.rs
+++ src/utils/debugging.rs
@@ -1,18 +1,18 @@
 use std::ffi::{CStr, CString};
 
-use ash::{
-    Entry,
-    version::EntryV1_0
-};
+use ash::{version::EntryV1_0, Entry};
 
 const VALIDATION_LAYERS: [&str; 1] = ["VK_LAYER_KHRONOS_validation"];
 
 pub fn check_validation_layer_support(entry: &Entry) -> bool {
     let layer_properties = match entry.enumerate_instance_layer_properties() {
         Ok(layer_properties) => layer_properties,
-        Err(e) => panic!("Failed to enumerate instance layer properties: {}", e)
+        Err(e) => panic!("Failed to enumerate instance layer properties: {}", e),
     };
-    let layer_names = layer_properties.iter().map(|lp| unsafe { CStr::from_ptr(lp.layer_name.as_ptr()).to_str().unwrap() }).collect::<Vec<&str>>();
+    let layer_names = layer_properties
+        .iter()
+        .map(|lp| unsafe { CStr::from_ptr(lp.layer_name.as_ptr()).to_str().unwrap() })
+        .collect::<Vec<&str>>();
     for validation_layer in VALIDATION_LAYERS.iter() {
         if !layer_names.contains(validation_layer) {
             return false;
@@ -22,8 +22,8 @@ pub fn check_validation_layer_support(entry: &Entry) -> bool {
 }
 
 pub fn get_enabled_layer_names() -> Vec<CString> {
-   VALIDATION_LAYERS
-    .iter()
-    .map(|layer_name| CString::new(*layer_name).unwrap())
-    .collect()
+    VALIDATION_LAYERS
+        .iter()
+        .map(|layer_name| CString::new(*layer_name).unwrap())
+        .collect()
 }
